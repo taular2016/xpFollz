f1965dffb48a5b60a35bb9b73d9bbe5aac3ad062
Luis Nunez <lan.alberto.vkrum@gmail.com>
2015-10-31 16:41:18 -0600

    adding remove circles and player list functionalities

---
+++ api/controllers/CircleController.js
@@ -0,0 +1,11 @@
+/**
+ * CircleController
+ *
+ * @description :: Server-side logic for managing circles
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
