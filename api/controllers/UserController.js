bb22429dc9abe5555e836e71a72a767b49190bbc
Luis Nunez <lan.alberto.vkrum@gmail.com>
2015-11-07 16:08:43 -0600

    adding restart game

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -11,5 +11,18 @@ module.exports = {
 		socket.join('players');
 		socket.join('game');
 		return response.ok('Subscribed to rooms players and game!');
+	},
+	restartGame: function(request, response, next) {
+		User.destroy().exec(function(error) {
+			if (error) {
+				return next(error);
+			}
+			Circle.destroy().exec(function(error) {
+				if (error) {
+					return next(error);
+				}
+				response.ok('Game restarted!');
+			});
+		});
 	}
 };
